6413e49c2efe6859a93e4dea2c94f819c0740785
Reymundo Tenorio <reymundotenorio@gmail.com>
2020-04-15 12:23:46 -0600

    Creating component WeatherExtraInfo

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import WeatherTemperature from './WeatherTemperature';
+import WeatherExtraInfo from './WeatherExtraInfo';
 
-const WeatherData = () => <div>Weather Data</div>;
+const WeatherData = () => (
+  <div>
+    <WeatherTemperature />
+    <WeatherExtraInfo />
+  </div>
+);
 
 export default WeatherData;
---
+++ src/components/WeatherExtraInfo.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const WeatherExtraInfo = () => <div>Extra Info</div>;
+
+export default WeatherExtraInfo;
