529b9fc01cc54a9531eaa602de085dd9f7b9d790
Reymundo Tenorio <reymundotenorio@gmail.com>
2020-04-18 23:46:46 -0600

    Refactoring components

--- src/components/WeatherLocation/WeatherData/index.js
+++ src/components/WeatherLocation/WeatherData/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import WeatherTemperature from './WeatherTemperature';
 import WeatherExtraInfo from './WeatherExtraInfo';
-import { CLOUD, CLOUDY, SUN, RAIN, SNOW, WINDY } from '../constants/weathers';
+import { CLOUD, CLOUDY, SUN, RAIN, SNOW, WINDY } from '../../constants/weathers';
 
 const WeatherData = () => (
   <div>
